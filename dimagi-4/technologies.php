f25bf739ee6ea4784861eac98526b2a219b62093
Biyeun Buczyk <biyeun@dimagi.com>
2013-03-08 18:44:15 -0500

    updated commcare blurb in technologies

--- dimagi-4/technologies.php
+++ dimagi-4/technologies.php
@@ -21,8 +21,8 @@ get_header(); ?>
 			<div class="well" style="height:100px;">
 	            <a href="<?php the_permalink(); ?>"><img src="<?php bloginfo('template_directory'); ?>/img/misc/technologies_commcare_thumb.png" alt="CommCare icon" /></a>
 	            <div class="pull-right span4 desaturate-links" style="width: 296px;">
-		            <h2><a href="<?php the_permalink(); ?>">CommCare</a></h2>
-		            <p><a href="<?php the_permalink(); ?>">Something about CommCare.</a></p>
+		            <h2><a href="http://www.commcarehq.org/">CommCare</a></h2>
+		            <p><a href="http://www.commcarehq.org/">CommCare is an easily customizable mobile platform for frontline workers to track and support their interactions with clients.</a></p>
 	            </div>
 	        </div>
 	    </div>
