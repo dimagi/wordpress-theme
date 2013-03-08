bbd03bd7b39a7109a3eeb71a88e20f74859fd302
Biyeun Buczyk <biyeun@dimagi.com>
2013-03-08 18:57:38 -0500

    more modifications to fab file

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,12 @@ from fabric import utils
 env.code_repo = 'https://github.com/dimagi/wordpress-theme.git'
 env.code_branch = 'master'
 env.root = root = '/home/content/d/i/m/dimagi'
-env.theme_root = theme = _join(root, 'html/wp/wp-content/themes')
+env.repo_root = repo = _join(root, 'sites/wordpress-theme')
+env.repo_theme = _join(repo, 'dimagi-4')
+theme = _join(root, 'html/wp/wp-content/themes')
 env.sudo_user = 'dimagi'
 env.hosts = ['dimagi.com']
+env.environment = 'production'
 
 
 def _join(*args):
@@ -16,18 +19,18 @@ def _join(*args):
     """
     return '/'.join(args)
 
+
 def production():
     """ use production environment on dimagi.com"""
-    env.repo_root = repo = _join(root, 'sites/wordpress-theme')
     env.theme_root = _join(theme, 'dimagi-4')
-    env.environment = 'production'
     env.user = prompt("Username: ", default=env.user)
+    
     
 def staging():
     """ use staging environment on dimagi.com"""
-    env.repo_root = repo = _join(root, 'sites/wordpress-theme-staging')
     env.theme_root = _join(theme, 'dimagi-beta')
     env.environment = 'staging'
+    env.code_branch = 'staging'
     env.user = prompt("Username: ", default=env.user)
 
 
@@ -39,27 +42,7 @@ def deploy():
             utils.abort('Production deployment aborted.')
 
     with cd(env.repo_root):
-      sudo('git pull --rebase', user=env.sudo_user)
-      sudo('cp ', user=env.sudo_user)
-        
- 
-    with cd(env.theme_root):
-      sudo('rm -r output', user=env.sudo_user)
-      sudo('rm -r tmp', user=env.sudo_user)
-      sudo('nanoc compile', user=env.sudo_user)
-
-
-def test():
-    """ deploy code to remote host by checking out the latest via git """
-    require('root', provided_by=('production'))
-    if env.environment == 'production':
-        if not console.confirm('Are you sure you want to deploy from github to production?', default=False):
-            utils.abort('Production deployment aborted.')
-
-    with cd(env.root):
-        sudo('git pull --rebase', user=env.sudo_user)
- 
-    with cd(env.site_root):
-      sudo('rm -r output', user=env.sudo_user)
-      sudo('rm -r tmp', user=env.sudo_user)
-      sudo('nanoc compile', user=env.sudo_user)
+      sudo('git checkout %(code_branch)s' % env, user=env.sudo_user)
+      sudo('git pull --rebase origin %(code_branch)s' % env, user=env.sudo_user)
+      sudo('rm -r %(theme_root)s' % env, user=env.sudo_user)
+      sudo('cp -r %(repo_theme)s %(theme_root)s' % env, user=env.sudo_user)
